02511f7898b777932e6c947a35b30187bfe28b28
Tobias Szczepanski <tob.ias.szczepanski@gmail.com>
2019-12-05 15:59:19 +0100

    PD-BMAN-9: Add bookmark overview Display bookmarks in a table Allow to delete individual bookmarks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from '../redux';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {StoreModule} from '@ngrx/store';
+import {metaReducers, reducers} from '../redux';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
 import {
   MatButtonModule,
   MatDialogModule,
   MatFormFieldModule,
   MatIconModule,
   MatInputModule,
+  MatTableModule,
   MatToolbarModule
 } from "@angular/material";
-import { BookmarkFormDialogComponent } from './bookmark-form-dialog/bookmark-form-dialog.component';
-import {FormsModule} from "@angular/forms";
+import {BookmarkFormDialogComponent} from './bookmark-form-dialog/bookmark-form-dialog.component';
+import {BookmarkOverviewComponent} from './bookmark-overview/bookmark-overview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookmarkFormDialogComponent
+    BookmarkFormDialogComponent,
+    BookmarkOverviewComponent
   ],
   entryComponents: [BookmarkFormDialogComponent],
   imports: [
@@ -41,9 +44,11 @@ import {FormsModule} from "@angular/forms";
     MatDialogModule,
     MatFormFieldModule,
     FormsModule,
-    MatInputModule
+    MatInputModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/bookmark-overview/bookmark-overview.component.ts
@@ -0,0 +1,41 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {bookmarksFeatureKey, BookmarkState, selectAll} from "../../redux/bookmark/bookmark.reducer";
+import {createFeatureSelector, select, Store} from "@ngrx/store";
+import {MatSort, MatTableDataSource} from "@angular/material";
+import {Observable} from "rxjs";
+import {Bookmark} from "../../redux/bookmark/bookmark.model";
+import {map} from "rxjs/operators";
+import {deleteBookmark} from "../../redux/bookmark/bookmark.actions";
+
+@Component({
+  selector: 'app-bookmark-overview',
+  templateUrl: './bookmark-overview.component.html',
+  styleUrls: ['./bookmark-overview.component.scss']
+})
+export class BookmarkOverviewComponent implements OnInit {
+
+  @ViewChild(MatSort, {static: true})
+  private matSort: MatSort;
+
+  displayedColumns = ["name", "url", "group", "delete"];
+
+  bookmarkDataSource$: Observable<MatTableDataSource<Bookmark>> = this.store.pipe(
+      select(createFeatureSelector(bookmarksFeatureKey)),
+      select(selectAll),
+      map((bookmarks: Bookmark[]) => {
+        const dataSource = new MatTableDataSource(bookmarks);
+        dataSource.sort = this.matSort;
+        return dataSource;
+      })
+  );
+
+  constructor(private store: Store<BookmarkState>,) {
+  }
+
+  ngOnInit() {
+  }
+
+  delete(id: string) {
+    this.store.dispatch(deleteBookmark({id}));
+  }
+}
