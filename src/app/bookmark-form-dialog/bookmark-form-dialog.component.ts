89606a0a3a2c0293b7e67b7901cdea843fbd00ae
Tobias Szczepanski <tob.ias.szczepanski@gmail.com>
2019-12-05 20:40:52 +0100

    PD-BMAN-12: Add form validation

--- src/app/bookmark-form-dialog/bookmark-form-dialog.component.ts
+++ src/app/bookmark-form-dialog/bookmark-form-dialog.component.ts
@@ -4,6 +4,7 @@ import {Bookmark} from "../../redux/bookmark/bookmark.model";
 import {Store} from "@ngrx/store";
 import {BookmarkState} from "../../redux/bookmark/bookmark.reducer";
 import {upsertBookmark} from "../../redux/bookmark/bookmark.actions";
+import {AbstractControl, FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-bookmark-form-dialog',
@@ -12,6 +13,10 @@ import {upsertBookmark} from "../../redux/bookmark/bookmark.actions";
 })
 export class BookmarkFormDialogComponent {
 
+  private name = new FormControl(this._bookmark.name, Validators.required);
+  private url = new FormControl(this._bookmark.url, [Validators.required, urlValidator]);
+  private group = new FormControl(this._bookmark.group);
+
   constructor(
       private store: Store<BookmarkState>,
       @Inject(MAT_DIALOG_DATA) private _bookmark: Bookmark
@@ -19,10 +24,26 @@ export class BookmarkFormDialogComponent {
   }
 
   upsertBookmark() {
-    this.store.dispatch(upsertBookmark({bookmark: this._bookmark}))
+    this._bookmark.name = this.name.value;
+    this._bookmark.url = this.url.value;
+    this._bookmark.group = this.group.value;
+
+    this.store.dispatch(upsertBookmark({bookmark: this.bookmark}))
   }
 
   get bookmark(): Bookmark {
     return this._bookmark;
   }
+}
+
+/**
+ * URL regex from @diegoperini
+ * @see https://mathiasbynens.be/demo/url-regex
+ */
+function urlValidator(control: AbstractControl): null | { [key: string]: any } {
+  const regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x00a1-\xffff0-9]+-?)*[a-z\x00a1-\xffff0-9]+)(?:\.(?:[a-z\x00a1-\xffff0-9]+-?)*[a-z\x00a1-\xffff0-9]+)*(?:\.(?:[a-z\x00a1-\xffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/ius;
+
+  return regex.test(control.value)
+      ? null
+      : {'urlPattern': 'Please enter a valid url'};
 }
